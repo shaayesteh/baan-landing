66ec43f649b6d919ca1603e0c82e03db428dd46d
shayesteh <schaayesteh@gmail.com>
2025-05-24 16:59:31 +0330

    remove unused scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,6 @@ export default function RootLayout({
       </head>
       <body className={`${vazirmatn.variable} antialiased font-bold`}>
         {children}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
       </body>
     </html>
   );
